4c4954d84563ed9682c7449bf1bbacf80d457fe4
mandapranathi11 <140315215+mandapranathi11@users.noreply.github.com>
2024-06-19 13:37:11 +0530

    Initial commit

---
+++ client/src/components/layout/Header.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { orange } from "../../constants/color";
+import {
+  Add as AddIcon,
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  Group as GroupIcon,
+} from "@mui/icons-material";
+
+const Header = () => {
+  const handleMobile = () => {
+    console.log("Mobile");
+  };
+  const openSearchDialog = () => {
+    console.log("openSearchDialog");
+  };
+  const openNewGroup = () => {
+    console.log("openNewGroup");
+  };
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar
+          position="static"
+          sx={{
+            bgcolor: orange,
+          }}
+        >
+          <Toolbar>
+            <Typography
+              variant="h6"
+              sx={{
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              Pandu
+            </Typography>
+
+            <Box
+              sx={{
+                display: { xs: "block", sm: "none" },
+              }}
+            >
+              <IconButton color="inherit" onClick={handleMobile}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box>
+              <Tooltip title="Search">
+                <IconButton
+                  color="inherit"
+                  size="large"
+                  onClick={openSearchDialog}
+                >
+                  <SearchIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="New Group">
+                <IconButton color="inherit" size="large" onClick={openNewGroup}>
+                  <AddIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Manage Groups">
+                <IconButton color="inherit" size="large" onClick={navigateToGroup}>
+                  <GroupIcon />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
